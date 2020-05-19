8672751caeaa37888dcaeb444beefdb4757bdcbf
Alex Shader <pixel84@gmail.com>
2020-05-19 15:48:09 +0300

    Initial Commit

---
+++ gitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitApp
+//
+//  Created by Alex Shader on 19.05.2020.
+//  Copyright © 2020 Alex Shader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
